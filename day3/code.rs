2d4fea14575b5b53d755a69b14e8c27c27738954
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2022-12-04 02:33:28 +1100

    Update code.rs

--- day3/code.rs
+++ day3/code.rs
@@ -5,14 +5,18 @@ use std::path::Path;
 fn main() {
     let mut result1 = 0 as i32;
     let mut result2 = 0 as i32;
+    let target2 = calculate_target(2);
+    let target3 = calculate_target(3);
+    println!(target2);
+    println!(target3);
     // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines("./input.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(s) = line {
                 let ba = s.as_bytes();
-                result1 = result1 + process(ba, 2);
-                result2 = result2 + process(ba, 3);
+                result1 = result1 + process(ba, 2, target2);
+                result2 = result2 + process(ba, 3, target3);
             }
         }
     }
@@ -28,16 +32,15 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn process(s: &[u8], parts: i32) -> i32 {
-    let mut target = calculate_target(parts);
+fn process(s: &[u8], parts: i32, target: i32) -> i32 {
     println!("{}", target);
     return s.len() as i32 + parts;
 }
 
 fn calculate_target(parts: i32) -> i32 {
     let mut total:i32 = 0;
-    for n in 1..parts {
-        total = total + n;
+    for n in 0..parts {
+        total = total + n + 1;
     }
     return total;
 }
