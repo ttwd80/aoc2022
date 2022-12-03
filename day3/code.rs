470aad9e02b4c2237fdd261b419d08d563c8115d
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2022-12-04 03:02:51 +1100

    Update code.rs

--- day3/code.rs
+++ day3/code.rs
@@ -32,9 +32,9 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn process(s: &[u8], const parts: i32, target: i32) -> i32 {
+fn process(s: &[u8], parts: i32, target: i32) -> i32 {
     let mut array: [u8; 128] = [0; 128];
-    let mut marker: [u8; parts] = [0; parts];
+    let mut marker = vec![0 as u8; parts];
     let mut m = 1;
     for n in 0..parts {
         marker[n] = m;
