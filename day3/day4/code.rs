06cc3c2965d1472bec9c1b1709aa94667e7e72c2
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2022-12-04 17:45:27 +1100

    Update code.rs

--- day3/day4/code.rs
+++ day3/day4/code.rs
@@ -1,1 +1,15 @@
-X
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    if let Ok(lines) = read_lines("./input.txt") {
+        
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
